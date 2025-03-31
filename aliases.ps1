147b719e03c1a9d70526c1ec9d6ff836ed48766b
Nuzkito <nuzkito+github@gmail.com>
2025-03-31 23:38:36 +0200

    Add aliases.

--- aliases.ps1
+++ aliases.ps1
@@ -1,7 +1,10 @@
 function s { git status $args }
 function u { git push origin main $args }
-function c {
+function m {
     git add .
     git commit -m $args
 }
 function l { git log $args }
+function c { composer $args }
+function a { php artisan $args }
+function t { php artisan test $args }
